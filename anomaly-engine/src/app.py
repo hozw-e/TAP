28b6e5d27b13528c35cfa5902049068345a67c5d
hozw-e <espiritujoshuapaolo@gmail.com>
2026-07-30 22:14:54 +0800

    fix: anomaly engine

--- anomaly-engine/src/app.py
+++ anomaly-engine/src/app.py
@@ -37,5 +37,7 @@ def create_app():
 
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
